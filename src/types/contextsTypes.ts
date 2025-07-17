18717de93b2105883e79a3a642a5aaf33cbbb6f4
CruelSun0 <jailsonsunshime@gmail.com>
2025-07-17 17:09:46 -0300

    Criando tipagem do MessageContext

--- src/types/contextsTypes.ts
+++ src/types/contextsTypes.ts
@@ -1,7 +1,7 @@
 export interface stateTypeQuizContext {
 
-    nivel:string | null,
-    categoria:string | null
+    nivel:string,
+    categoria:string
 
 }
 
@@ -11,3 +11,26 @@ export interface actionTypeQuizContext {
     payload: string
 
 }
+
+export interface stateQuizGameContext {
+
+    questaoAtual: number,
+    alternativaSelecionada:null | 0 | 1 | 2 | 3,
+    alternativaCorreta:boolean | null,
+    acertos:number,
+    erros:number,
+
+}
+
+export type actionQuizGameContext  =  {type:'PROXIMA_QUESTAO'} | 
+{type:'SET_ALTERNATIVA', payload:0 | 1 | 2 | 3} | 
+{type:'SET_ALTERNATIVA_CORRETA', payload:boolean}
+
+export interface stateMessageContext {
+
+    mostrarMensagem:boolean
+
+}
+
+export type actionMessageContext  = {type:'MOSTRAR'} | {type:'ESCONDER'}
+
